e764d65ca97e50701f374b76d50e9a1a478fd4a7
thewidesky <3332370021@qq.com>
2025-02-25 11:16:55 +0800

    1

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 DB_CONFIG = {
+    "mysql_path": "C://Program Files//MySQL/MySQL Server 8.0//bin//mysql.exe",
     "host": "localhost",
     "user": "root",
-    "password": "123456",
+    "password": "58072151",
     "database": "ecommerce_db",
     "port": 3306
 }
